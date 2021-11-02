8afb1b60f68d1390c897a97989042b2b64631bef
Jon Walls <jon@jonathanwalls.com>
2021-11-02 23:15:01 +0000

    update basket_id to be a unique field by concat with household_id

--- models/warehouse/fct_purchased_items.sql
+++ models/warehouse/fct_purchased_items.sql
@@ -7,7 +7,7 @@ renamed as (
     select
         concat(cast(household_key as string), '-', cast (basket_id as string), '-', cast (product_id as string)) as pk_purchased_items,
         household_key as household_id,
-        basket_id,
+        concat(cast(household_id as string), '-', cast(basket_id as string)),
         {{ get_demo_duration() }} - day as days_ago,
         date_sub(current_date(), interval ({{ get_demo_duration() }} - day) day) as transaction_date,
         product_id,
